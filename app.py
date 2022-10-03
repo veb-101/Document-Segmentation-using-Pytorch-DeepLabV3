cf4939d0997c4be2e762d05186d24bb3100ec0bd
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2022-10-03 18:46:13 +0530

    test 1 fix  torch installation problem

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ try:
     import torch
 
 # This block executes only on the first run when your package isn't installed
-except ModuleNotFoundError as e:
+except Exception as e:
     subprocess.Popen([f"{sys.executable} -m pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cpu"], shell=True)
     # wait for subprocess to install package before running your actual code below
     time.sleep(50)
